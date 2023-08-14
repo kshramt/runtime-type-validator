54fdfd8bcd2870ec0fa727ed4519275a484cafbf
kshramt <kshramt@example.com>
2023-08-14 22:38:00 +0900

    Revert "Simplify inferred types"
    This reverts commit f287f5a67b8480df3d1a6950a79df1158a1a6479.
    
    It was not useful for larger types.

--- src/index.ts
+++ src/index.ts
@@ -230,10 +230,7 @@ export const $record = <K extends string, VV extends TValidator<unknown>>(
 export const $object = <Kvs extends Record<string, TValidator<unknown>>>(
   kvs: Kvs,
 ) => {
-  return (
-    value: unknown,
-    path: TRef<TPath>,
-  ): value is TMerge<TInferValues<Kvs>> => {
+  return (value: unknown, path: TRef<TPath>): value is TInferValues<Kvs> => {
     if (!isObject(value)) {
       path.value = { not_object: value };
       return false;
